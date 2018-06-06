3d5c9931968e5c90f72f955224d0497dec24d8eb
Juan Sebastian Conde Farias <jsconde08@misena.edu.co>
2018-06-06 11:25:54 -0500

    Update index.php
    Se cambio el tipo de gráfica :V

--- index.php
+++ index.php
@@ -1,34 +1,82 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Pruebas gráficas</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
-    <script src="http://code.highcharts.com/highcharts.js"></script>
-    <script src="http://code.highcharts.com/modules/exporting.js"></script>
-</head>
-<body>
-    <div id="grafica">
-        <script>
-            $(function($)
-            {
-                $('#grafica').highcharts({
-                        title:{text:'Prueba de una gráfica'},
-                        xAxis:{categories:['BOGDB01', 'PERDB01', 'BAQDB01']},
-                        yAxis:{title:'%'}, plotlines:[{value:0,width:1, color:'red'}],
-                        tooltip:{valueSuffix:'%'},
-                        legend:{layout:'vertical',align:'right',verticalAlign:'middle',borderWidth:0},
-                        series:[{name:'asd',data:[2,25,75]}]
-                });
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Prueba gráfica</title>
 
-            })        
-        </script>
+		<style type="text/css">
+
+		</style>
+	</head>
+	<body>
+<script src="Highcharts/code/highcharts.js"></script>
+<script src="Highcharts/code/modules/exporting.js"></script>
+<script src="Highcharts/code/modules/export-data.js"></script>
+
+<div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
 
 
 
-    </div>
-    
-</body>
+		<script type="text/javascript">
+
+Highcharts.chart('container', {
+    chart: {
+        type: 'area'
+    },
+    title: {
+        text: 'Prueba de una gráfica'
+    },
+    subtitle: {
+        text: 'Servidores'
+    },
+    xAxis: {
+        allowDecimals: false,
+        labels: {
+            formatter: function () {
+                return this.value; // clean, unformatted number for year
+            }
+        }
+    },
+    yAxis: {
+        title: {
+            text: 'Nucleos de cpu'
+        },
+        labels: {
+            formatter: function () {
+                return this.value / 1 + 'CPU';
+            }
+        }
+    },
+    tooltip: {
+        pointFormat: '{series.name} had stockpiled <b>{point.y:,.0f}</b><br/>warheads in {point.x}'
+    },
+    plotOptions: {
+        area: {
+            pointStart: 1,
+            marker: {
+                enabled: false,
+                symbol: 'circle',
+                radius: 2,
+                states: {
+                    hover: {
+                        enabled: true
+                    }
+                }
+            }
+        }
+    },
+    series: [{
+        name: 'BOGDB01',
+        data: [ 10,20,30,40,50 ]
+    }, {
+        name: 'PERDB01',
+        data: [ 20,60,80 ]
+    }, {
+        name: 'BAQDB01',
+        data: [ 50,70,10,130 ]    
+    }]
+});
+		</script>
+	</body>
 </html>
